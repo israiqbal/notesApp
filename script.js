c003a9dd8fdd95f9b439f1a431e5f29da17196ac
israiqbal <israiqbal2018@gmail.com>
2025-11-25 20:47:44 +0530

    Implement random password generation and clipboard copy
    Refactor script.js to implement random password generation functions and event listeners for generating and copying passwords.

--- script.js
+++ script.js
@@ -1,97 +1,86 @@
-const addBtn = document.querySelector("#addBtn");
-const main = document.querySelector("#main");
+// Functions for generating random number lowercase uppercase letters , symbols
 
-// Click event listener
-addBtn.addEventListener("click", function () {
-    addNote();
+/* Math.random method genrate a random floating-point numbers
+Math.floor() function returns the largest integer less than or equal to a given number.
+For generating a random uppercase lowercase text random numbers symbols we use Charcode 
+http://stevehardie.com/2009/09/character-code-list-char-code/ */
+
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+  return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+  const symbols = "!@#$%^&*(){}[]=<>/,.";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+// adding a all functions into a object called randomFunc
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol,
+};
+
+// adding a click event listner to generate button
+const generate = document.getElementById("generateBtn");
+generate.addEventListener("click", () => {
+  const length = document.getElementById("Passwordlength").value;
+  const hasUpper = document.getElementById("uppercase").checked;
+  const hasLower = document.getElementById("lowercase").checked;
+  const hasNumber = document.getElementById("numbers").checked;
+  const hasSymbol = document.getElementById("symbols").checked;
+  const result = document.getElementById("PasswordResult");
+  result.innerText = generatePassword(
+    hasLower,
+    hasUpper,
+    hasNumber,
+    hasSymbol,
+    length
+  );
+  // console.log(hasLower, hasUpper, hasNumber, hasSymbol);
 });
 
-// Save button function
-const saveNotes = () => {
-
-    // Select content textareas
-    const notes = 
-        document.querySelectorAll(".note .content"); 
-        
-    // Select title textareas
-    const titles = 
-        document.querySelectorAll(".note .title"); 
-
-    const data = [];
-
-    notes.forEach((note, index) => {
-        const content = note.value;
-        const title = titles[index].value;
-        console.log(title);
-        if (content.trim() !== "") {
-            data.push({ title, content });
-        }
-    });
-
-    const titlesData = 
-        data.map((item) => item.title);
-    console.log(titlesData);
-    localStorage.setItem(
-        "titles", JSON.stringify(titlesData));
-
-    const contentData = 
-        data.map((item) => item.content);
-    localStorage.setItem(
-        "notes", JSON.stringify(contentData));
-};
-
-// Addnote button function
-const addNote = (text = "", title = "") => {
-    const note = document.createElement("div");
-    note.classList.add("note");
-    note.innerHTML = `
-    <div class="icons">
-         <i class="save fas fa-save" 
-             style="color:red">
-         </i>
-         <i class="trash fas fa-trash" 
-             style="color:yellow">
-         </i> 
-    </div>
-    <div class="title-div">
-        <textarea class="title" 
-            placeholder="Write the title ...">${title}
-        </textarea>
-    </div>
-    <textarea class="content" 
-        placeholder="Note down your thoughts ...">${text}
-    </textarea>
-    `;
-    function handleTrashClick() {
-        note.remove();
-        saveNotes();
+// function for generating random password
+function generatePassword(lower, upper, number, symbol, length) {
+    let generatedPassword = "";
+    const typesCount = lower + upper + number + symbol;
+    // filter out unchecked types
+    const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(
+      (item) => Object.values(item)[0]
+    );
+    // console.log(typesArr);
+  
+    // creating a loop for calling generator function for each type
+    for (let i = 0; i < length; i += typesCount) {
+      typesArr.forEach((type) => {
+        const funcName = Object.keys(type)[0];
+        generatedPassword += randomFunc[funcName]();
+      });
     }
-    function handleSaveClick() {
-        saveNotes();
-    }
-    const delBtn = note.querySelector(".trash");
-    const saveButton = note.querySelector(".save");
-    const textareas = note.querySelectorAll("textarea");
-
-    delBtn.addEventListener("click", handleTrashClick);
-    saveButton.addEventListener("click", handleSaveClick);
-    main.appendChild(note);
-    saveNotes();
-};
-
-// Loading all the notes those are saved in 
-// the localstorage
-function loadNotes() {
-
-    const titlesData = 
-        JSON.parse(localStorage.getItem("titles")) || [];
-    const contentData = 
-        JSON.parse(localStorage.getItem("notes")) || [];
-        
-    for (let i = 0; 
-            i < Math.max(
-                titlesData.length, contentData.length); i++) {
-        addNote(contentData[i], titlesData[i]);
-    }
-}
-loadNotes();
+  
+    // slicing password from 0 to length
+    const finalPassword = generatedPassword.slice(0, length);
+    return finalPassword;
+  }
+  
+  // copy to clipboard
+let button = document.getElementById("clipboardBtn");
+// add click event listner on button
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  // execute command for copy text by selecting textarea text with id
+  document.execCommand(
+    "copy",
+    false,
+    document.getElementById("PasswordResult").select()
+  );
+});
